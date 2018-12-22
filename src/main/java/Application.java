4e80c5daeeafcca5f307835891f49c0138a52d1a
prokhorovartem <art.98.pro@gmail.com>
2018-12-23 02:04:08 +0300

    added simple integrator

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,8 +1,9 @@
 import input.InputModel;
 import input.ReaderFromFile;
+import integrator.IntegrationModel;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println(InputModel.createModel(ReaderFromFile.readExpression()));
+        System.out.println(IntegrationModel.integrateExpression(InputModel.createModel(ReaderFromFile.readExpression())));
     }
 }
--- src/main/java/integrator/IntegrationModel.java
+++ src/main/java/integrator/IntegrationModel.java
@@ -1,5 +1,34 @@
 package integrator;
 
-public class IntegrationModel {
+import input.operand.BinaryOperator;
+import input.operand.Number;
+import input.operand.Operand;
+import input.operand.Variable;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class IntegrationModel {
+    public static List<Operand> integrateExpression(List<Operand> operands) {
+        List<Operand> integratedOperands = new ArrayList<>();
+        for (Operand operand : operands) {
+            if (operand instanceof Number) {
+                integratedOperands.add(operand);
+                integratedOperands.add(BinaryOperator.MULTIPLY);
+                integratedOperands.add(Variable.X);
+            }
+            if (operand instanceof BinaryOperator){
+                if (Objects.equals(operand, BinaryOperator.PLUS))
+                    integratedOperands.add(BinaryOperator.PLUS);
+                if (Objects.equals(operand, BinaryOperator.MINUS))
+                    integratedOperands.add(BinaryOperator.MINUS);
+                if (Objects.equals(operand, BinaryOperator.MULTIPLY))
+                    integratedOperands.add(BinaryOperator.MULTIPLY);
+                if (Objects.equals(operand, BinaryOperator.DIVIDE))
+                    integratedOperands.add(BinaryOperator.DIVIDE);
+            }
+        }
+        return integratedOperands;
+    }
 }
