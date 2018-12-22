edc1e8db991b38c7ba9086410a4bb7e357a84a59
prokhorovartem <art.98.pro@gmail.com>
2018-12-23 00:49:09 +0300

    added number and divided sign classes

---
+++ src/main/java/input/DividedSign.java
@@ -0,0 +1,15 @@
+package input;
+
+import java.util.Map;
+
+public class DividedSign {
+    private Map<Number, Number> dividedNumber;
+
+    public DividedSign(Map<Number, Number> dividedNumber) {
+        this.dividedNumber = dividedNumber;
+    }
+
+    public Map<Number, Number> getDividedNumber() {
+        return dividedNumber;
+    }
+}
---
+++ src/main/java/input/Number.java
@@ -0,0 +1,13 @@
+package input;
+
+public class Number {
+    private Double number;
+
+    public Number(Double number) {
+        this.number = number;
+    }
+
+    public Double getNumber() {
+        return number;
+    }
+}
