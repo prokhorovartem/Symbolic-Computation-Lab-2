bb956eb5b14897fa0bb8a472cd6edea7cc1796a5
r.goroshko <r.goroshko@yandex.ru>
2019-01-22 19:53:48 +0300

    x^n special case

--- src/main/java/symbolic/visitor/impl/VisitorImpl.java
+++ src/main/java/symbolic/visitor/impl/VisitorImpl.java
@@ -2,6 +2,7 @@ package symbolic.visitor.impl;
 
 import symbolic.model.Expression;
 import symbolic.model.Operation;
+import symbolic.model.OperationType;
 import symbolic.model.impl.Variable;
 import symbolic.visitor.Visitor;
 import symbolic.worker.Dispatcher;
@@ -18,11 +19,15 @@ public class VisitorImpl implements Visitor {
     public Expression visit(Operation operation) {
         Dispatcher dispatcher = new Dispatcher();
         Resolver resolver = new Resolver();
-        return dispatcher.resolveOperation(
-                operation.getOperationType(),
-                resolver.resolveExpression(operation.getFirstArgument()),
-                (operation.getSecondArgument() != null)
-                        ? resolver.resolveExpression(operation.getSecondArgument()) : null
-        );
+        if (OperationType.POW.equals(operation.getOperationType()) && operation.getFirstArgument().isVariable()) {
+            return dispatcher.resolveOperation(operation.getOperationType(), operation.getFirstArgument(), operation.getSecondArgument());
+        } else {
+            return dispatcher.resolveOperation(
+                    operation.getOperationType(),
+                    resolver.resolveExpression(operation.getFirstArgument()),
+                    (operation.getSecondArgument() != null)
+                            ? resolver.resolveExpression(operation.getSecondArgument()) : null
+            );
+        }
     }
 }
