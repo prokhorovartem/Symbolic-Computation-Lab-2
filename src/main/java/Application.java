e45e5319ad497a04be88b088a872c8d8688bb0aa
Prokhorov Artem <art.98.pro@gmail.com>
2019-01-24 16:04:43 +0600

    added plot

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -3,12 +3,23 @@ import io.InputModel;
 import io.OutputModel;
 import io.Resource;
 import io.model.InputExpression;
+import javafx.scene.Scene;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 import symbolic.model.Expression;
 import symbolic.visitor.impl.Resolver;
 
 import java.util.List;
 
-public class Application {
+public class Application extends javafx.application.Application {
     public static void main(String[] args) {
         Resource inputResource = new Resource(args[0]);
         Resource outputResource = new Resource(args[1]);
@@ -25,6 +36,61 @@ public class Application {
         OutputModel outputModel = new OutputModel(outputResource);
         outputModel.printResult(resolvedExpression);
 
+        launch(args);
 
     }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+        TextField leftBorderTextField = new TextField("-1");
+        TextField rightBorderTextField = new TextField("1");
+        Button btn = new Button("Change borders");
+
+        final NumberAxis xAxis1 = new NumberAxis();
+        final NumberAxis yAxis1 = new NumberAxis();
+        final LineChart<Number, Number> lineChart1 = new LineChart<>(xAxis1, yAxis1);
+        XYChart.Series series1 = new XYChart.Series();
+        series1.setName("F(x)");
+        lineChart1.getData().add(series1);
+        btn.setOnAction(event -> {
+            try {
+                double leftBorder = Double.parseDouble(leftBorderTextField.getText());
+
+                double rightBorder = Double.parseDouble(rightBorderTextField.getText());
+
+                if (rightBorder < leftBorder)
+                    throw new NumberFormatException("left border should be less than right border");
+                initData(series1, leftBorder, rightBorder);
+            } catch (NumberFormatException ex) {
+                System.out.println(ex.toString());
+            }
+        });
+
+        VBox vBox1 = new VBox();
+        vBox1.getChildren().addAll(lineChart1);
+
+        HBox chartBox = new HBox();
+        chartBox.getChildren().addAll(vBox1);
+
+        VBox vBox = new VBox();
+        vBox.getChildren().addAll(leftBorderTextField, rightBorderTextField, btn, chartBox);
+
+        StackPane root = new StackPane();
+        root.getChildren().add(vBox);
+
+        Scene scene = new Scene(root, 505, 550);
+
+        primaryStage.setTitle("Symbolic Computation, Lab4");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    private void initData(XYChart.Series series1, double leftBorder, double rightBorder) {
+        series1.getData().remove(0, series1.getData().size());
+        for (double i = leftBorder; i < rightBorder; i += 0.05) {
+            series1.getData().add(new XYChart.Data(i, Math.sin(i)));
+        }
+    }
+
 }
